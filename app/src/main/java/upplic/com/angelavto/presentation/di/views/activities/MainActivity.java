d87163848ac03676af97ad1522d8db91b089ee50
Vladislav_Aniskin <vladyaaifree@gmail.com>
2016-07-04 10:44:36 +0300

    first commit

---
+++ app/src/main/java/upplic/com/angelavto/presentation/di/views/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package upplic.com.angelavto.presentation.di.views.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import upplic.com.angelavto.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ac_main);
+    }
+}
